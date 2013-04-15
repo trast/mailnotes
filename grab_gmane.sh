ecdd50c3d0249ea7a131aa745eee9c1cd84ecc1a
Thomas Rast <trast@inf.ethz.ch>
2013-04-15 15:16:50 +0200

    GMane downloader script
    With an important fix over the old one: will not silently overwrite
    the existing downloaded file with one that only contains a single
    mail!

---
+++ grab_gmane.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# config
+GMANE_DROPBOX=~/dl/gmane-git
+REPO=~/tmp/git
+
+# download new mails
+cd "$GMANE_DROPBOX"
+cur_inbox=$(ls | sort -n | tail -1)
+cur_gmane_id () {
+    grep '^Archived-At' "$1" | tail -1 | sed 's#.*/\([0-9]\{1,\}\)>$#\1#'
+}
+begin=$(($(cur_gmane_id $cur_inbox) + 1))
+boundary=$(($begin+1000))
+wget -O gmane-$boundary.tmp http://download.gmane.org/gmane.comp.version-control.git/$begin/$boundary
+end=$(cur_gmane_id gmane-$boundary.tmp)
+if [ -z "$end" ]; then
+    rm gmane-$boundary.tmp
+else
+    mv gmane-$boundary.tmp $end
+fi
